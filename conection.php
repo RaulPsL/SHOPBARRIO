3390f04c5291f5948ba37ff059d1071d04fed7e5
CarlosV <vargascarlos12123@gmail.com>
2023-03-25 15:21:05 -0400

    se modifico el archivo conection.php

--- conection.php
+++ conection.php
@@ -1,1 +1,15 @@
-Yo me conectare a la base de datos
+<?php
+$servername = "127.0.0.1:3306";
+$username = "u341591096_prueba";
+$password = "Contraseña1";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+  echo "fallo la coneccion";
+}
+echo "Connected successfully";
+?>
