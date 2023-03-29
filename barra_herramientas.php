17a3ab0541e1475e9a316b9a319bf8ba7ab21265
CarlosV <vargascarlos12123@gmail.com>
2023-03-29 00:23:50 -0400

    se creo el footer, header y barra de herramientas

---
+++ barra_herramientas.php
@@ -0,0 +1,32 @@
+<?php include_once 'back/venta-historico.php'; ?>
+<?php 
+$pagina=basename($_SERVER['PHP_SELF']);
+switch ($pagina) {
+    case "vender.php":
+        ?>
+        <div class="barra-herramientas">
+            <label class="venta-historico">Total vendido: Bs. <?php echo $venta_historico; ?></label>
+        </div>
+        <?php
+        break;
+
+    case "productos.php":
+        ?>
+        <div class="barra-herramientas">
+            <label class="venta-historico">Otra cosa></label>
+        </div>
+        <?php
+        break;
+
+    // Agregar más casos según sea necesario
+
+    default:
+        // Código a ejecutar si no se cumple ningún caso anterior
+        ?>
+        <div class="barra-herramientas">
+            <label>no hay nada></label>
+        </div>
+        <?php
+        break;
+}
+?>
--- menu_izq.php
+++ menu_izq.php
@@ -1,43 +1,31 @@
-<?php
-include_once 'back/datos_tienda.php';
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Shopbarrio</title>
-    <link rel="stylesheet" href="css/style_menu.css">
-    <link rel="shortcut icon" href="src/pan.jpg"> 
-    <script src="js/script_menu.js"></script>
-  </head>
-  <body>
-    <div class="menu">
-        <div class="menu-title">           
-            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-            <lottie-player class="menu-gif" src="https://assets1.lottiefiles.com/packages/lf20_xavpbl2r.json"  background="transparent"  speed="1" loop  autoplay></lottie-player>        
-            
-            <h1><?php echo $nombre; ?></h1>
+<?php include_once 'back/datos_tienda.php'; ?>
+<div class="menu">
+    <div class="menu-title">           
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <lottie-player class="menu-gif" src="https://assets1.lottiefiles.com/packages/lf20_xavpbl2r.json"  background="transparent"  speed="1" loop  autoplay></lottie-player>        
+        
+        <h1><?php echo $tienda; ?></h1>
+    </div>
+    <div class="menu-content">
+        <div class="menu-content-area areas-subareas">
+            <img src="src/icon-product.svg">
+            <h2>Productos</h2>
+            <img class="icon-area-0 " src="src/icon-down.svg">
+            <img class="icon-area-0 ocultar" src="src/icon-up.svg">
         </div>
-        <div class="menu-content">
-            <div class="menu-content-area areas-subareas">
-                <img src="src/icon-product.svg">
-                <h2>Productos</h2>
-                <img class="icon-area-0 " src="src/icon-down.svg">
-                <img class="icon-area-0 ocultar" src="src/icon-up.svg">
-            </div>
-            <div class="menu-content-subarea area-0 ocultar-flex">
-                <img src="src/icon-arrow.svg">
-                <a href="#"><h3>Todos los productos</h3></a>
-            </div>
-            <div class="menu-content-subarea area-0 ocultar-flex"   >
-                <img src="src/icon-arrow.svg">
-                <a href="#"><h3>Nuevo producto</h3></a>
-            </div>
-            <div onclick="abrirPagina('/#')" class="menu-content-area">
-                <img src="src/icon-vender.svg">
-                <h2>Vender</h2>
-                <img src="src/icon-down.svg" style="opacity:0;">
-            </div>
+        <div class="menu-content-subarea area-0 ocultar-flex">
+            <img src="src/icon-arrow.svg">
+            <a href="#"><h3>Todos los productos</h3></a>
+        </div>
+        <div class="menu-content-subarea area-0 ocultar-flex"   >
+            <img src="src/icon-arrow.svg">
+            <a href="#"><h3>Nuevo producto</h3></a>
+        </div>
+        <div onclick="abrirPagina('/#')" class="menu-content-area">
+            <img src="src/icon-vender.svg">
+            <h2>Vender</h2>
+            <img src="src/icon-down.svg" style="opacity:0;">
         </div>
     </div>
+</div>
     
