5131a71cd0594c42ff3733127a219e44d04de08b
Sol Marquez <63815258+pastelito7u7@users.noreply.github.com>
2023-04-01 23:54:17 -0400

    modificacion para subir imagenes a la base

--- back/datos_registrar.php
+++ back/datos_registrar.php
@@ -9,8 +9,20 @@ include_once dirname(__DIR__) . '/conection.php';
     $cantidad = $_POST["cantidad"] ;
     $descripcion = $_POST["descripcion"] ;
 
+    // Datos de la imagen 
+    $imagen = $_FILES['subir']['name'];
+    $tipo =  $_FILES['subir']['type'];
+    $temp =  $_FILES['subir']['tmp_name'];
 
-    $sql = "INSERT INTO `PRODUCTO`(`ID_TIENDA`, `ID_CATEGORIA`, `NOMBRE_PRODUCTO`, `PRECIOV_PRODUCTO`, `PRECIOC_PRODUCTO`, `VENCIMIENTO_PRODUCTO`, `STOCK_PRODUCTO`, `IMAGEN_PRODUCTO`, `DESCRIPCION_PRODUCTO`) VALUES ('1','1','$nombre','$venta','$compra','$fecha','$cantidad','imagen','$descripcion')";
+    //ruta
+    $carpeta='uploads/user_1';
+    $ruta = $carpeta.'/'.$imagen;
+    $carpeta='../uploads/user_1';
+
+    move_uploaded_file($temp,$carpeta.'/'.$imagen);
+
+
+    $sql = "INSERT INTO `PRODUCTO`(`ID_TIENDA`, `ID_CATEGORIA`, `NOMBRE_PRODUCTO`, `PRECIOV_PRODUCTO`, `PRECIOC_PRODUCTO`, `VENCIMIENTO_PRODUCTO`, `STOCK_PRODUCTO`, `IMAGEN_PRODUCTO`, `DESCRIPCION_PRODUCTO`) VALUES ('1',$categoria,'$nombre','$venta','$compra','$fecha','$cantidad','$ruta','$descripcion')";
 
     $query = mysqli_query($conn, $sql);
 
@@ -19,7 +31,6 @@ include_once dirname(__DIR__) . '/conection.php';
     }else{
         echo "incorrecto";
     }
-
 
 
 
