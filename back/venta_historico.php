c18afc555fe667be6408e833f15fd2ae7a448ba7
RaulPsL <125601312+RaulPsL@users.noreply.github.com>
2023-04-09 21:00:20 -0400

    Commit for RaulPsL

--- back/venta_historico.php
+++ back/venta_historico.php
@@ -2,15 +2,15 @@
 include_once dirname(__DIR__) . '/conection.php';
 
 // Ejecutar una consulta SELECT
-$sql = "SELECT NOMBRE_TIENDA FROM TIENDA WHERE ID_TIENDA = 1";
+$sql = "SELECT SUM(TOTAL_VENTA) AS TOTAL FROM VENTA WHERE ID_TIENDA = 1";
 $resultado = mysqli_query($conn, $sql);
 
 // Verificar si se encontraron resultados
 if (mysqli_num_rows($resultado) > 0) {
     // Recorrer los resultados y mostrarlos
     while($fila = mysqli_fetch_assoc($resultado)) {
-        echo "<script>console.log('Nombre: " . $fila['NOMBRE_TIENDA'] . "')</script>";
-        $tienda=$fila['NOMBRE_TIENDA'];
+        echo "<script>console.log('Nombre: " . $fila['TOTAL'] . "')</script>";
+        $venta_historico=$fila['TOTAL'];
     }
 } else {
     echo "<script>console.log('No se encontraron resultados')</script>";
